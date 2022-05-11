213ee3b5e8f900db1d057600b376f50de2237244
Katsuyuki Nishoji <katsuyuki.nishoji@crowdworks.co.jp>
2022-05-11 14:33:03 +0900

    wip

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "1.0.10"
+  required_version = "1.1.9"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.64.2"
+      version = "4.13.0"
     }
     random = {
       source  = "hashicorp/random"
